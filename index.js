cd602bde6c3b130ef01b5ce948649de10620e256
anisamuema <anisa.muema.school@gmail.com>
2023-09-22 22:06:38 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+let divide = function(){
+ let divide = (num1, num2) => num2/num1;
+ let result = divide(100,2000);
+ return result;
+};
+
+let square = (x) =>{
+ return x*x;
+}
+
+let add = (a, b) =>{
+ return a+b;
+}
+    
